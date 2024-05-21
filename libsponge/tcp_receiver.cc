9dca44db8a0735ec06d6a6193528553800f3de2d
yudong1013 <chen11206@outlook.com>
2024-05-20 23:14:06 -0700

    lab2 completed

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -10,10 +10,34 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
+// Find the correct first_index of the segment and write the segment to the byte stream
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    const auto &header = seg.header();
+    
+    if (!_isn.has_value()) { // no SYN received before
+        if (!header.syn) return; // if the current segment is not SYN, discard it
+        _isn = header.seqno; // if the current segment is SYN, the seqno is isn
+    }
+    
+    uint64_t checkpoint = _reassembler.stream_out().bytes_written(); // index of the last reassmebled byte
+    uint64_t abs_seqno = unwrap(header.seqno, _isn.value(), checkpoint);
+    uint64_t stream_index = abs_seqno - 1 + (header.syn ? 1: 0); // the same only if the current segment is SYN, otherwise increase by 1 for the SYN processed before
+    _reassembler.push_substring(seg.payload().copy(), stream_index, header.fin); // FIN signals eof
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+// Return the expected ackno and check if SYN has been received
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    if (!_isn.has_value()) return nullopt; // no ACK if no SYN received before
+    
+    // Ackno is the first unassmebled byte, indicating where the next segment should be placed
+    uint64_t abs_seq = _reassembler.stream_out().bytes_written() + 1 + // +1 for SYN; byte_written is equal to the index of the next byte expected
+                    (_reassembler.stream_out().input_ended() ? 1 : 0); // if input ended, add 1 for the FIN
+    
+    return wrap(abs_seq, _isn.value());
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const {
+    // window size is the distance between the first unassmebled byte (ackno) and the last byte that can be accepted (eof)
+    // ackno + window size defines the range of indices that TCP sender can send
+    return _capacity - _reassembler.stream_out().buffer_size();
+}
--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -20,12 +20,17 @@ class TCPReceiver {
     //! The maximum number of bytes we'll store.
     size_t _capacity;
 
+    // The initial sequence number of the sender.
+    // All SYN segments have seqno as isn
+    // If it has no value, then we haven't received a SYN yet; SYN flag is 0
+    std::optional<WrappingInt32> _isn;
+
   public:
     //! \brief Construct a TCP receiver
     //!
     //! \param capacity the maximum number of bytes that the receiver will
     //!                 store in its buffers at any give time.
-    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity) {}
+    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity), _isn() {}
 
     //! \name Accessors to provide feedback to the remote TCPSender
     //!@{
